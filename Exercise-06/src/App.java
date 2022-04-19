b9684774bc84d6bb706933a8fbf0a15afe1207e7
LuizFJP <luizfjportela@gmail.com>
2022-04-19 15:41:42 -0300

    feat: exercise-06

--- Exercise-06/src/App.java
+++ Exercise-06/src/App.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        
+      Scanner keyboard = new Scanner(System.in);
+
+      String name = keyboard.next();
+      double salary = keyboard.nextDouble();
+      double financing = keyboard.nextDouble();
+
+      System.out.println(name);
+      if (financing <= salary * 5) {
+        System.out.println("Financiamento CONCEDIDO");
+      } else {
+        System.out.println("Financiamento NEGADO");
+      }
+
+      System.out.println("Obrigado por nos consultar");
+      keyboard.close();
     }
 }
